ee00267b2b2a7bf6f04bf23271dc5fffcb3d708f
Xfanxx <anshorifuat12@gmail.com>
2022-09-22 01:28:16 +0700

    22092022

--- orders/views.py
+++ orders/views.py
@@ -68,4 +68,7 @@ def place_order(request,total=0,quantity=0):
 
 
 def payments(request):
-    return render(request,'orders/payments.html')
+    context={
+        'title':'Payment | page',
+    }
+    return render(request,'orders/payments.html',context)
